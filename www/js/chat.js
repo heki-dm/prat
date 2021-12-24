70e8ed545074d5c2e65df13683fdf8acaee4fa30
Heki <kakeru.u.se@gmail.com>
2021-12-24 12:50:31 +0900

    fix: extends削除

--- www/js/chat.js
+++ www/js/chat.js
@@ -3,7 +3,7 @@
  */
 console.log('chat.js is ready!')
 
-class Chat extends App {
+class Chat{
 	constructor() {
 		// NCMB初期化
 		this.APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
