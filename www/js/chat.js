40bc391ef97a00a1ed005489d2cf32d97859fa5e
Heki <kakeru.u.se@gmail.com>
2021-12-31 12:26:25 +0900

    fix: undefind is not an object

--- www/js/chat.js
+++ www/js/chat.js
@@ -3,17 +3,17 @@
  */
 console.log('chat.js is ready!')
 
-// NCMB初期化
-var APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
-var CLIENTKEY = '4fc1b2b336e3edbd6d39f308008bea45066c89b4d02b00242bf9118d4e195faa'
-var Chat_ncmb = ncmb.DataStore('User')
-var chat = Chat_ncmb()
 
 class Chat {
 	constructor() {
 		this.id
 		this.name
 		this.member
+		// NCMB初期化
+		var APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
+		var CLIENTKEY = '4fc1b2b336e3edbd6d39f308008bea45066c89b4d02b00242bf9118d4e195faa'
+		var Chat_ncmb = ncmb.DataStore('User')
+		var chat = Chat_ncmb()
 	}
 
 	addRoom = (name, member) => {
--- www/js/user.js
+++ www/js/user.js
@@ -3,18 +3,18 @@
  */
 console.log('user.js is ready!');
 
-// NCMB初期化
-var APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
-var CLIENTKEY = '4fc1b2b336e3edbd6d39f308008bea45066c89b4d02b00242bf9118d4e195faa'
-let ncmb = new NCMB(APPKEY, CLIENTKEY)
-var User_ncmb = ncmb.DataStore('User')
-var user_ncmb = User_ncmb()
 
 class User {
 	constructor(userName, password) {
 		this.id
 		this.name = userName
 		this.password = password
+		// NCMB初期化
+		var APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
+		var CLIENTKEY = '4fc1b2b336e3edbd6d39f308008bea45066c89b4d02b00242bf9118d4e195faa'
+		let ncmb = new NCMB(APPKEY, CLIENTKEY)
+		var User_ncmb = ncmb.DataStore('User')
+		var user_ncmb = User_ncmb()
 	}
 
 	/**
