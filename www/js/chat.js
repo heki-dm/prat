d1408f5d5c16630385d33eb43a82bae806555a32
Heki <kakeru.u.se@gmail.com>
2021-12-31 12:22:46 +0900

    fix: undefind is not an object

--- www/js/chat.js
+++ www/js/chat.js
@@ -6,8 +6,8 @@ console.log('chat.js is ready!')
 // NCMB初期化
 var APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
 var CLIENTKEY = '4fc1b2b336e3edbd6d39f308008bea45066c89b4d02b00242bf9118d4e195faa'
-const Chat_ncmb = ncmb.DataStore('User')
-const chat = Chat_ncmb()
+var Chat_ncmb = ncmb.DataStore('User')
+var chat = Chat_ncmb()
 
 class Chat {
 	constructor() {
--- www/js/user.js
+++ www/js/user.js
@@ -7,8 +7,8 @@ console.log('user.js is ready!');
 var APPKEY = 'f0e44ccb004cf57d62815c9c90b3ec503f1d2b8320a001fc36956368ead57581'
 var CLIENTKEY = '4fc1b2b336e3edbd6d39f308008bea45066c89b4d02b00242bf9118d4e195faa'
 let ncmb = new NCMB(APPKEY, CLIENTKEY)
-const User_ncmb = ncmb.DataStore('User')
-const user_ncmb = User_ncmb()
+var User_ncmb = ncmb.DataStore('User')
+var user_ncmb = User_ncmb()
 
 class User {
 	constructor(userName, password) {
